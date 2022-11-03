fed8ba5a463625c1dd24f71994616cf389e43ea8
V3L0C1T13S <turtletester20@gmail.com>
2022-11-02 23:22:14 -0400

    feat: Stub servers.json

---
+++ docs/discovery.md
@@ -0,0 +1,3 @@
+# Revolt Discovery
+
+Revolt discovery is the offical, proprietary discovery feature for Revolt. This document outlines some of the routes that discovery uses, how it works, and why.
--- src/api/routes/discover.ts
+++ src/api/routes/discover.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import servers from "../../common/db/servers.json";
 
 const router = express.Router();
 
-router.get("/discover/servers", (req, res) => {
-  res.send("OK");
+router.get("/discover/servers.json", (req, res) => {
+  res.json(servers);
 });
 
 export default router;
